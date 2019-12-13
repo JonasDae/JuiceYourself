9864214a5dcc09616bb2c00fe1282ce6941101f3
r0699038 <r0699038@student.thomasmore.be>
2019-12-13 15:34:45 +0100

    Afwerking home scherm en linken naar volgende activitys

--- app/src/main/java/be/thomasmore/juiceyourself/MainActivity.java
+++ app/src/main/java/be/thomasmore/juiceyourself/MainActivity.java
@@ -2,7 +2,9 @@ package be.thomasmore.juiceyourself;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void search_onClick(View v) {
+        Intent intent = new Intent(this, Search.class);
+        startActivity(intent);
+    }
+
+    public void add_onClick(View v) {
+        Intent intent = new Intent(this, New.class);
+        startActivity(intent);
+    }
+
+    public void top_onClick(View v) {
+        Intent intent = new Intent(this, Highscore.class);
+        startActivity(intent);
+    }
+
+    public void counter_onClick(View v) {
+        Intent intent = new Intent(this, Counter.class);
+        startActivity(intent);
     }
 }
