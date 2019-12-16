b4a0259fc94c379255af7285c004a1c6e3407ac9
jonasDae <r0463164@student.thomasmore.be>
2019-12-16 14:57:17 +0100

    fix imports

--- app/src/main/java/be/thomasmore/juiceyourself/MainActivity.java
+++ app/src/main/java/be/thomasmore/juiceyourself/MainActivity.java
@@ -1,7 +1,11 @@
 package be.thomasmore.juiceyourself;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import java.util.List;
 
