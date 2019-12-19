e2724427eb2a86abcfcb8093483f54672b62c12d
r0699038 <r0699038@student.thomasmore.be>
2019-12-19 23:57:39 +0100

    menu fix

--- app/src/main/java/be/thomasmore/juiceyourself/Highscore.java
+++ app/src/main/java/be/thomasmore/juiceyourself/Highscore.java
@@ -57,10 +57,12 @@ public class Highscore extends AppCompatActivity {
             case R.id.menu_counter:
                 counter_onClick();
                 return true;
+            case R.id.menu_hulp:
+                hulp_onClick();
+                return true;
             default:
                 return false;
         }
-
     }
 
     //Menu views
@@ -86,6 +88,11 @@ public class Highscore extends AppCompatActivity {
 
     public void counter_onClick() {
         Intent intent = new Intent(this, Counter.class);
+        startActivity(intent);
+    }
+
+    public void hulp_onClick() {
+        Intent intent = new Intent(this, Hulp.class);
         startActivity(intent);
     }
 }
