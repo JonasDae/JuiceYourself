3c7c7cd66b5191617cf349b4bb1aaa7717cbac6d
r0699038 <r0699038@student.thomasmore.be>
2019-12-19 19:13:00 +0100

    update

--- app/src/main/java/be/thomasmore/juiceyourself/Search.java
+++ app/src/main/java/be/thomasmore/juiceyourself/Search.java
@@ -53,6 +53,11 @@ public class Search extends AppCompatActivity {
 
     }
 
+    public void searchResult_onClick(View v) {
+        Intent intent = new Intent(this, SearchResult.class);
+        startActivity(intent);
+    }
+
     //Menu views
     public void home_onClick() {
         Intent intent = new Intent(this, MainActivity.class);
